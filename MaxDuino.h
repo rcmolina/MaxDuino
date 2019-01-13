ae24301ce9133cb707ee079a2a7cd6072538e104
rcmolina <rcmolina@gmail.com>
2019-01-13 22:45:22 +0100

    Add files via upload

--- MaxDuino.h
+++ MaxDuino.h
@@ -2,9 +2,11 @@
 #define outputPin           9
 
 #ifdef MINIDUINO_AMPLI
-  #define INIT_OUTPORT         DDRB |= B00000011       // pin8+ pin9 es el bit0-bit1 del PORTB 
-  #define WRITE_LOW           PORTB = (PORTB & B11111101) | B00000001       // pin8+ pin9 , bit0- bit1 del PORTB
-  #define WRITE_HIGH          PORTB = (PORTB | B00000010) & B11111110       // pin8+ pin9 , bit0- bit1 del PORTB
+  #define INIT_OUTPORT         DDRB |= B00000011                              // pin8+ pin9 es el bit0-bit1 del PORTB 
+  #define WRITE_LOW           (PORTB &= B11111101) |= B00000001               // pin8+ pin9 , bit0- bit1 del PORTB
+  #define WRITE_HIGH          (PORTB |= B00000010) &= B11111110               // pin8+ pin9 , bit0- bit1 del PORTB
+//  #define WRITE_LOW           PORTB = (PORTB & B11111101) | B00000001         // pin8+ pin9 , bit0- bit1 del PORTB
+//  #define WRITE_HIGH          PORTB = (PORTB | B00000010) & B11111110         // pin8+ pin9 , bit0- bit1 del PORTB   
 #else
   #define INIT_OUTPORT         DDRB |=  _BV(1)         // El pin9 es el bit1 del PORTB
   #define WRITE_LOW           PORTB &= ~_BV(1)         // El pin9 es el bit1 del PORTB
