4ea7c6b80dd82a93ee1d5e80ccd80ac176aad145
Dave Hooper <dave@beermex.com>
2024-12-17 00:31:03 +0000

    small tidy up

--- MaxDuino/CounterPercent.cpp
+++ MaxDuino/CounterPercent.cpp
@@ -1,11 +1,9 @@
 #include "configs.h"
 #include "Arduino.h"
+#include "compat.h"
 #include "Display.h"
 #include "utils.h"
 #include "file_utils.h"
-
-//extern char* utoa( unsigned int value, char *string, int radix ) ;
-#include "compat.h"
 
 byte currpct = 100;
 unsigned int lcdsegs = 0;
