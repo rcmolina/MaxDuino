b5f8b60d429ac700745d5419a6561872ef5e532c
Rafa Molina <rcmolina@gmail.com>
2024-12-21 10:09:07 +0100

    Fix for config var file name
    Remove underscore

--- MaxDuino/userconfig.h
+++ MaxDuino/userconfig.h
@@ -3,7 +3,7 @@
 // You can also just #define CONFIG_FILE in configs.h
 // or #define one of the per-plaform CONFIG_FILE_DEFAULT settings instead (see #defines_config.h)
 // or if you want you can still define it here - but this will only be used for ATMEGA328P boards
-#ifndef CONFIG_FILE
-#define CONFIG_FILE 7
+#ifndef CONFIGFILE
+#define CONFIGFILE 7
 #endif
 
